ab8a82370cad1a39ab55f7f21e9e7156e5592124
Malte Bargholz <malte@screenri.de>
2020-04-01 22:15:28 +0200

    generate.sh timestamp and gitstamp rocket-dir prior to evaluation

--- zedboard/generate-evals.sh
+++ zedboard/generate-evals.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-mkdir -p evals/
+timestamp=$(date '+%d.%m.%y_%H:%M:%S')
+commit=$(cd ../rocket-chip && git rev-parse --short HEAD)
+base="evals/eval_${commit}_${timestamp}"
+echo $base
 
-HARTS_SYNTH="2 4 8 16"
+mkdir -p "$base"
+
+HARTS_SYNTH="2 4 8 16 32"
 HARTS_IMPL="2 4"
 IRQS="8 16 32 64 128"
 
@@ -15,7 +20,7 @@ do
         unset FPGA_CONFIG
         config_str="EvalConfigH${h}I${i}"
         FPGA_CONFIG="$config_str" make synth_utilization
-        eval_dir="evals/synth/${config_str}"
+        eval_dir="$base/synth/${config_str}"
         gen_dir="zedboard_rocketchip_$config_str"
         mkdir -p "$eval_dir"
         mv -f "${config_str}_util_synth_plic.txt" "$eval_dir/util_plic.txt"
@@ -35,7 +40,7 @@ do
         unset FPGA_CONFIG
         config_str="EvalConfigH${h}I${i}"
         FPGA_CONFIG="$config_str" make impl_utilization
-        eval_dir="evals/impl/${config_str}"
+        eval_dir="$base/impl/${config_str}"
         gen_dir="zedboard_rocketchip_$config_str"
         mkdir -p "$eval_dir"
         mv -f "${config_str}_util_impl_plic.txt" "${eval_dir}/util_plic.txt"
