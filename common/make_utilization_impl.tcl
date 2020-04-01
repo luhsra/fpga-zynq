31cd7929494b984da9ff99d27a430a7eae8f68b8
Malte Bargholz <malte@screenri.de>
2020-04-01 20:48:37 +0200

    generate timing reports for impl

--- common/make_utilization_impl.tcl
+++ common/make_utilization_impl.tcl
@@ -8,4 +8,6 @@ wait_on_run impl_1
 open_run impl_1
 report_utilization -cells top/target/plic -file CHISEL_CONFIG_HERE_util_impl_plic.txt
 report_utilization -cells top/target -file CHISEL_CONFIG_HERE_util_impl_rocket.txt
+report_timing_summary -delay_type min_max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -routable_nets -cells [get_cells top/target/plic] -name timing_1 -file CHISEL_CONFIG_HERE_timing_impl_plic.txt
+report_timing_summary -delay_type min_max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -routable_nets -name timing_full -file CHISEL_CONFIG_HERE_timing_impl_rocket.txt
 exit
